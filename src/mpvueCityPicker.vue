4af0e868429cdd385230d3182e9599b7ee1b86ef
KuangPF <1633397286@qq.com>
2018-08-28 23:40:57 +0800

    feature:init mpvueCityPicker

---
+++ src/mpvueCityPicker.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="mpvue-picker">
+    <div :class="{'pickerMask':showPicker}" @click="maskClick" catchtouchmove="true"></div>
+    <div class="mpvue-picker-content " :class="{'mpvue-picker-view-show':showPicker}">
+      <div class="mpvue-picker__hd">
+        <div class="mpvue-picker__action" @click="pickerCancel">取消</div>
+        <div class="mpvue-picker__action" @click="pickerConfirm">确定</div>
+      </div>
+      <!-- 三级联动 -->
+      <picker-view indicator-style="height: 40px;" class="mpvue-picker-view">
+        <block>
+          <picker-view-column>
+            <div class="picker-item" v-for="(item,index) in provinceDataList" :key="index">{{item}}</div>
+          </picker-view-column>
+          <picker-view-column>
+            <div class="picker-item" v-for="(item,index) in cityDataList" :key="index">{{item}}</div>
+          </picker-view-column>
+          <picker-view-column>
+            <div class="picker-item" v-for="(item,index) in areaDataList" :key="index">{{item}}</div>
+          </picker-view-column>
+        </block>
+      </picker-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      provinceDataList: [1, 2, 3],
+      cityDataList: [1, 2, 3],
+      areaDataList: [3, 4, 5]
+    };
+  },
+  props: {
+    /* 是否显示控件 */
+    showPicker: {
+      type: Boolean,
+      default: false
+    },
+    /* 默认值 */
+    pickerValueDefault: {
+      type: Array,
+      default: [0, 0, 0]
+    }
+  },
+  methods: {
+    show() {
+      setTimeout(() => {
+        this.showPicker = true;
+      }, 0);
+    },
+    maskClick() {
+      this.pickerCancel();
+    },
+    pickerCancel() {
+      this.showPicker = false;
+    },
+    pickerConfirm(e) {
+      this.showPicker = false;
+    },
+    showPickerView() {
+      this.showPicker = true;
+    },
+    pickerChange(e) {
+      this.pickerValue = e.mp.detail.value;
+    }
+  }
+};
+</script>
+
+<style>
+.pickerMask {
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.6);
+}
+.mpvue-picker-content {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  transition: all 0.3s ease;
+  transform: translateY(100%);
+  z-index: 3000;
+}
+.mpvue-picker-view-show {
+  transform: translateY(0);
+}
+.mpvue-picker__hd {
+  display: flex;
+  padding: 9px 15px;
+  background-color: #fff;
+  position: relative;
+  text-align: center;
+  font-size: 17px;
+}
+.mpvue-picker__hd:after {
+  content: ' ';
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  height: 1px;
+  border-bottom: 1px solid #e5e5e5;
+  color: #e5e5e5;
+  transform-origin: 0 100%;
+  transform: scaleY(0.5);
+}
+.mpvue-picker__action {
+  display: block;
+  flex: 1;
+  color: #1aad19;
+}
+.mpvue-picker__action:first-child {
+  text-align: left;
+  color: #888;
+}
+.mpvue-picker__action:last-child {
+  text-align: right;
+}
+.picker-item {
+  text-align: center;
+  line-height: 40px;
+}
+.mpvue-picker-view {
+  position: relative;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 238px;
+  background-color: rgba(255, 255, 255, 1);
+}
+</style>
--- test/pages/index/index.vue
+++ test/pages/index/index.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="test">mpvue-cityPicker</div>
+<div class="mpvue-city-picker">
+  <button type="default" @click="showCityPicker">城市选择</button>
+  <mpvue-city-picker ref="mpvueCityPicker"></mpvue-city-picker>
+</div>
 </template>
 
 <script>
+import mpvueCityPicker from '../../../src/mpvueCityPicker.vue';
 export default {
-
+  data() {},
+  components: {
+    mpvueCityPicker
+  },
+  methods: {
+    showCityPicker() {
+      this.$refs.mpvueCityPicker.show();
+    }
+  }
 };
 </script>
-
 <style>
-
 </style>
